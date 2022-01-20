8e744957be4a480bde00c33866eb44c439523b79
yuzuki-chi <47371712+yuzuki-chi@users.noreply.github.com>
2022-01-21 04:24:17 +0900

    set my script

---
+++ html/get-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while [ 1 ]
+do 
+  echo ""
+  echo "   ----------------------------------------------- "
+  echo ""
+  echo "    BE SURE TO CHECK THE SOURCE BEFORE RUN IT!  "
+  echo ""
+  echo "    Keep on drawing dreaming! https://yuzukichi.jp"
+  echo "    Follow @yuzukichu_lip on Twitter for updates!"
+  echo ""
+  echo "   ----------------------------------------------- "
+  echo ""
+done
